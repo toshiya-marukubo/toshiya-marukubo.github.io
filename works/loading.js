f80809eb6befdb1647479ccada64e8358602f036
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-01-09 22:33:01 +0900

    add loading.js

---
+++ works/loading.js
@@ -0,0 +1,10 @@
+(function(){
+  'use strict';
+  window.addEventListener('load', function() {
+    var ccanvas = document.getElementById('canvas');
+    var loading = document.getElementById('loading');
+    loading.style.display = 'none';
+    canvas.style.visibility = 'visible';
+  }, false);
+})();
+
