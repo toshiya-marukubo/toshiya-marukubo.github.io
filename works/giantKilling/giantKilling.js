1b061b359b329e3d27d8257e297f382b6e04ef14
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-01-22 18:55:10 +0900

    created giantKilling.js

--- works/giantKilling/giantKilling.js
+++ works/giantKilling/giantKilling.js
@@ -70,21 +70,54 @@
       };
       */
       this.c = {
-        r: rand(0, 0),
-        g: rand(0, 0),
-        b: rand(0, 0)
-      }
+        r: rand(255, 255),
+        g: rand(255, 255),
+        b: rand(255, 255)
+      };
+      this.r = ySplit / 3;
     };
 
     Circle.prototype.draw = function() {
       ctx = this.ctx;
-      ctx.globalCompositeOperation = 'xor';
-      ctx.globalAlpha = 0.8;
       ctx.beginPath();
       ctx.fillStyle = 'rgb(' + this.c.r + ',' + this.c.g + ',' + this.c.b + ')';
-      ctx.arc(this.x, this.y, ySplit / 3, 0, 2 * Math.PI);
+      ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
       ctx.closePath();
       ctx.fill();
+    };
+
+    Circle.prototype.updateParams = function(thatR) {
+      this.r += thatR / 3;
+    };
+
+    Circle.prototype.resize = function() {
+      this.x = rand(0, X);
+      this.y = rand(0, Y);
+    };
+
+    Circle.prototype.coll = function(i) {
+      var j = i;
+      for (var i = 0; i < circles.length; i++) {
+        if (j !== i) {
+          var a;
+          var b;
+          var c;
+          var thatR = circles[i].r;
+          var sumRadius = this.r + thatR;
+          a = this.x - circles[i].x;
+          b = this.y - circles[i].y;
+          c = a * a + b * b;
+          if (c <= sumRadius * sumRadius) {
+            if (this.r >= thatR) {
+              circles[i].updateParams(thatR);
+              circles.splice(j, 1);
+            } else {
+              circles[j].updateParams(thatR);
+              circles.splice(i, 1);
+            }
+          }
+        }
+      }
     };
     
     Circle.prototype.updatePosition = function() {
@@ -93,21 +126,23 @@
     };
 
     Circle.prototype.wrapPosition = function() {
-      if (this.x - ySplit / 3 > X) {
+      if (this.x - this.r > X) {
         this.x = 0;
       }
-      if (this.x + ySplit / 3 < 0) {
+      if (this.x + this.r < 0) {
         this.x = X;
       }
-      if (this.y - ySplit / 3 > Y) {
+      if (this.y - this.r > Y) {
         this.y = 0;
       }
-      if (this.y + ySplit / 3 < 0) {
+      if (this.y + this.r < 0) {
         this.y = Y;
       }
     };
 
-    Circle.prototype.render = function() {
+    Circle.prototype.render = function(i) {
+      this.updatePosition();
+      this.coll(i);
       this.wrapPosition();
       this.draw();
     };
@@ -123,10 +158,10 @@
       Render
     ********************/
     
-    function render(){
+    function render(i){
       ctx.clearRect(0, 0, X, Y);
       for (var i = 0; i < circles.length; i++) {
-        circles[i].render();
+        circles[i].render(i);
       }
       requestAnimationFrame(render);
     }
@@ -141,30 +176,25 @@
     function onResize() {
       X = canvas.width = window.innerWidth;
       Y = canvas.height = window.innerHeight;
-      xSplit = X / splitNum;
-      ySplit = Y / splitNum;
-      circles = [];
-      for (var i = 0; i < colMax; i++) {
-        for (var j = 0; j < rowMax; j++) {
-          var circle = new Circle(ctx, xSplit * i + xSplit / 2, ySplit * j + ySplit / 2);
-          circles.push(circle);
-        }
+      for (var i = 0; i < circles.length; i++) {
+        circles[i].resize();
       }
     }
-
+     
     window.addEventListener('mousemove', function(e) {
       var diff = e.clientX;
       distance = diff / 100;
       for (var i = 0; i < circles.length; i++) {
         circles[i].updatePosition();
+        circles[i].coll(i);
       }
     }, false);
-
+    
     window.addEventListener('resize', function() {
       onResize();
     });
 
   }); 
   // Author
-  console.log('File Name / unknownVer1.js\nCreated Date / January 22, 2020\nAuthor / Toshiya Marukubo\nTwitter / https://twitter.com/toshiyamarukubo');
+  console.log('File Name / giantKilling.js\nCreated Date / January 22, 2020\nAuthor / Toshiya Marukubo\nTwitter / https://twitter.com/toshiyamarukubo');
 })();
