3edaca5dc44b41f10501f4096fa129a96ef42999
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-01-24 16:03:55 +0900

    fixed home

---
+++ home/js/worksHover.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    var worksList = document.getElementById('worksList');
+    var dispWorks = document.getElementById('dispWorks');
+    var worksLists = worksList.children;
+
+    for (var i = 0; i < worksLists.length; i++) {
+      worksLists[i].addEventListener('mouseenter', openIframe, false);
+    }
+    for (var i = 0; i < worksLists.length; i++) {
+      worksLists[i].addEventListener('mouseleave', closeIframe, false);
+    }
+    function openIframe() {
+      var url = this.firstElementChild.href;
+      dispWorks.src = url;
+      dispWorks.style.display = 'block';
+    }
+    function closeIframe() {
+      dispWorks.style.display = 'none';
+      console.log('close');
+    }
+  });
+})();
