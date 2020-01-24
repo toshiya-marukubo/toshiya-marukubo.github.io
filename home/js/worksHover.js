e34d9c7561857a3be41ed387c363de7b0e30a224
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-01-24 16:17:23 +0900

    fixed

--- home/js/worksHover.js
+++ home/js/worksHover.js
@@ -1,9 +1,14 @@
 (function() {
   'use strict';
   window.addEventListener('load', function() {
+    var X = window.innerWidth;
     var worksList = document.getElementById('worksList');
     var dispWorks = document.getElementById('dispWorks');
     var worksLists = worksList.children;
+
+    if(X < 768) {
+      return;
+    }
 
     for (var i = 0; i < worksLists.length; i++) {
       worksLists[i].addEventListener('mouseenter', openIframe, false);
