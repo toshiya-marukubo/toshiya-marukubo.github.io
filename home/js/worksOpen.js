b80eff7dff3af4f51195d248747caab068e2c35d
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-03-04 20:42:31 +0900

    fixed index.html

--- home/js/worksOpen.js
+++ home/js/worksOpen.js
@@ -3,23 +3,28 @@
   window.addEventListener('load', function() {
     var worksBtn = document.getElementById('worksBtn');
     var works = document.getElementById('works');
-    var closeBtn = document.getElementById('close');
+    var about = document.getElementById('about');
+    var closeBtn = document.getElementsByClassName('close');
     
     worksBtn.addEventListener('click', function(e) {
       e.preventDefault();
       if (works.style.display === 'none') {
         works.style.display = 'block';
-        closeBtn.firstElementChild.focus();
+        about.style.display = 'none';
+        works.firstElementChild.firstElementChild.focus();
       } else {
         works.style.display = 'none';
         worksBtn.firstElementChild.focus();
       }
     }, false);
     
-    closeBtn.addEventListener('click', function(e) {
-      e.preventDefault();
-      works.style.display = 'none';
-      worksBtn.firstElementChild.focus();
-    }, false);
+    for (var i = 0; i < closeBtn.length; i++) {
+      closeBtn[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        works.style.display = 'none';
+        worksBtn.firstElementChild.focus();
+      }, false);
+    }
+     
   });
 })();
