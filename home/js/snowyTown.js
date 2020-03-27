7391077f52d34fd407d41bd13470893f7f73f411
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-03-27 18:45:48 +0900

    fixed index.html

--- home/js/snowyTown.js
+++ home/js/snowyTown.js
@@ -435,5 +435,5 @@
     
   });
   // Author
-  console.log('File Name / snowyTown.js\nCreated Date / 2019.12.10\nAuthor / Toshiya Marukubo\nTwitter / https://twitter.com/toshiyamarukubo');
+  console.log('Created Date / 2019.12.10\nAuthor / Toshiya Marukubo\nTwitter / https://twitter.com/toshiyamarukubo');
 })();
