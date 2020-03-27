bd160caad5ff78a9189b94a764ccdf0dd956d83d
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-03-27 17:09:00 +0900

    fixed index.html

---
+++ home/js/scrollFixed.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+  window.addEventListener('load', function() {
+    var canvasBack = document.getElementById('canvasBack');
+    window.addEventListener('scroll', function() {
+      console.log('scroll');
+    });
+  });
+})();
