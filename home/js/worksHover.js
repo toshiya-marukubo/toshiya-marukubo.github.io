62a9c7aa6c8d49e07a38f21387dcfae6c526524d
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-03-29 09:15:16 +0900

    fixed index.html

--- home/js/worksHover.js
+++ home/js/worksHover.js
@@ -27,7 +27,7 @@
       this.parentNode.setAttribute('class', 'opend');
       for (var i = 0; i < dispWorksChildren.length; i++) {
         if (dispWorksChildren[i].className !== 'opend') {
-          dispWorksChildren[i].style.opacity = '0';
+          dispWorksChildren[i].style.opacity = '0.1';
         }
       }
       var iframe = document.createElement('iframe');
