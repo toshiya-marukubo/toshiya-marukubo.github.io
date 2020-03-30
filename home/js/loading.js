e80248f8af904d8f4875a61ffc8ca7b10958fe4e
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-03-30 09:05:21 +0900

    fixed index.html

--- home/js/loading.js
+++ home/js/loading.js
@@ -4,10 +4,12 @@
     var main = document.getElementById('main');
     var footer = document.getElementById('footer');
     var loading = document.getElementById('loading');
-    loading.style.display = 'none';
-    main.style.display = 'block';
-    if (footer) {
-      footer.style.display = 'block';
-    }
+    setTimeout(function() {
+      loading.style.display = 'none';
+      main.style.display = 'block';
+      if (footer) {
+        footer.style.display = 'block';
+      }
+    }, 800);
   }, false);
 })();
