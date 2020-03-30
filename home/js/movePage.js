a12ccfc53c661d6b79d6be8e753fbb1b970d00e4
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-03-30 18:21:36 +0900

    fixed index.html

--- home/js/movePage.js
+++ home/js/movePage.js
@@ -1,16 +1,20 @@
 (function() {
   'use strict';
   window.addEventListener('load', function() {
-    var req = new XMLHttpRequest();
     var dispWorks = document.getElementById('dispWorks');
     var dispWorksChildren = dispWorks.children;
     var openHref = null;
     for (var i = 0; i < dispWorksChildren.length; i++) {
       dispWorksChildren[i].firstElementChild.addEventListener('mouseenter', function() {
+        var req = new XMLHttpRequest();
         openHref = this.href;
+        console.log(openHref);
         req.open('GET', openHref);
         req.responseType = 'document';
         req.send(null);
+        req.addEventListener('load', function() {
+          document.getElementsByTagName('html')[0].innerHTML = req.response;
+        }, false);
       }, false);
     }
     for (var i = 0; i < dispWorksChildren.length; i++) {
@@ -18,8 +22,5 @@
         openHref = null;
       }, false);
     }
-    req.addEventListener('load', function() {
-      document.getElementsByTagName('html')[0].innerHTML = req.response;
-    }, false);
   });
 })();
