597bd5a32a18e2c89dfae63f6f3fd4a89145742a
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-05-02 21:49:47 +0900

    fixed

--- home/js/loading.js
+++ home/js/loading.js
@@ -1,15 +1,12 @@
 (function(){
   'use strict';
-  window.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('load', function() {
     var main = document.getElementById('main');
     var footer = document.getElementById('footer');
     var loading = document.getElementById('loading');
     setTimeout(function() {
       loading.style.display = 'none';
       main.style.display = 'block';
-      if (footer) {
-        footer.style.display = 'block';
-      }
     }, 800);
   }, false);
 })();
