8aeb8492fa52d4d8dfc40c093c0e7b9a49302841
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-05-06 20:24:33 +0900

    fixed

--- home/js/fadeIn.js
+++ home/js/fadeIn.js
@@ -2,6 +2,7 @@
   'use strict';
   window.addEventListener('DOMContentLoaded', function() {
     var body = document.getElementsByTagName('body')[0];
+    body.style.display = 'block';
     body.setAttribute('class', 'fadeIn');
   });
 })();
