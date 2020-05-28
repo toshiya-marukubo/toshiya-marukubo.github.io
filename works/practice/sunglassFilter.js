86ef6b98804610abcd66961134afdc5f88d0c64a
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-05-28 16:41:30 +0900

    fixed

--- works/practice/sunglassFilter.js
+++ works/practice/sunglassFilter.js
@@ -6,18 +6,16 @@
           data = imagedata.data,
           length = data.length,
           width = imagedata.width;
-      for (var i = 0; i < length; i++) {
-        if (i = 0; i < length; i++) {
-          if ((i + 1) % 4 != 0) {
-            if ((i + 4) % (width * 4) == 0) {
-              data[i] = data[i - 4];
-              data[i + 1] = data[i - 3];
-              data[i + 2] = data[i - 2];
-              data[i + 3] = data[i - 1];
-              i += 4;
-            } else {
-              data[i] = 2 * data[i] - data[i + 4] - 0.5 * data[i + 4];
-            }
+      for (var i = 0; i < length; ++i) {
+        if ((i + 1) % 4 != 0) {
+          if ((i + 4) % (width * 4) == 0) {
+            data[i] = data[i - 4];
+            data[i + 1] = data[i - 3];
+            data[i + 2] = data[i - 2];
+            data[i + 3] = data[i - 1];
+            i += 4;
+          } else {
+            data[i] = 2 * data[i] - data[i + 4] - 0.5 * data[i + 4];
           }
         }
       }
