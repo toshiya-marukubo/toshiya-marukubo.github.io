d7bd4374aefb3ead64a12c27f48dfb664d52927f
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-07-17 22:35:37 +0900

    fixed

--- pattern/scripts/fadeInOut.js
+++ pattern/scripts/fadeInOut.js
@@ -26,7 +26,7 @@
       setTimeout(function() {
       //body.style.display = 'none';
         location.href = url;
-      }, 1000);
+      }, 500);
     }
 
   });
--- pattern/scripts/movePage.js
+++ pattern/scripts/movePage.js
@@ -37,7 +37,7 @@
       setTimeout(function() {
       //body.style.display = 'none';
         location.href = url;
-      }, 1000);
+      }, 500);
     }
 
     canvas.addEventListener('wheel', function(e) {
