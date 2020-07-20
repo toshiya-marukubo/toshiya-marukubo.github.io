443c5ab6b20a7d4b5a722aced1e99561af5c5dc2
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-07-20 11:46:26 +0900

    fixed

--- pattern/scripts/movePage.js
+++ pattern/scripts/movePage.js
@@ -11,8 +11,8 @@
     
     var body = document.getElementsByTagName('body')[0];
     var works = [
-      'gokuzushi'
-      'kikkouhanabishi'
+      'gokuzushi',
+      'kikkouhanabishi',
       'komochikikkou',
       'kikkoutsunagi',
       'amime',
