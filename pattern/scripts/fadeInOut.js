e30d040087a97b9f1a65cf7c500374341a731def
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-07-22 16:11:58 +0900

    fixed

--- pattern/scripts/fadeInOut.js
+++ pattern/scripts/fadeInOut.js
@@ -3,6 +3,7 @@
 * Created Date / Jun 15, 2020
 * Auther / Toshiya Marukubo
 * Twitter / https://twitter.com/toshiyamarukubo
+* Referenced / https://qiita.com/shibe23/items/79f950061457ff1a7687
 */
 
 (function() {
