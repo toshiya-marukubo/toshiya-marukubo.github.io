958fbc37374de199f3c1487d155338f29f0e700f
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-07-22 15:02:11 +0900

    added script.

--- pattern/scripts/fadeInOut.js
+++ pattern/scripts/fadeInOut.js
@@ -9,10 +9,15 @@
   'use strict';
   window.addEventListener('DOMContentLoaded', function() {
     var body = document.getElementsByTagName('body')[0];
+    var header = document.getElementsByTagName('header')[0];
+    var h2 = document.getElementsByTagName('h2')[0];
     var a = document.getElementsByTagName('a');
+    
     body.style.display = 'block';
     body.setAttribute('class', 'fadeIn');
-
+    header.setAttribute('class', 'onloadHeader');
+    if (h2) h2.setAttribute('class', 'onloadH2');
+    
     for (var i = 0; i < a.length; i++) {
       a[i].addEventListener('click', function(e) {
         e.preventDefault();
