36b3c5c759c38a4751a8c50b959839a9059baf8d
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-07-24 20:32:29 +0900

    fixed

--- pattern/works/asanoha/asanoha.js
+++ pattern/works/asanoha/asanoha.js
@@ -131,7 +131,7 @@
     Shape.prototype.updateParams = function() {
       this.a += 1;
       this.rad = this.a * Math.PI / 180;
-      this.r = Math.sin(this.rad) * height / 2;
+      Math.sin(this.rad) < 0 ? this.r = -Math.sin(this.rad) * height / 2 : this.r = Math.sin(this.rad) * height / 2;
     };
 
     Shape.prototype.render = function() {
