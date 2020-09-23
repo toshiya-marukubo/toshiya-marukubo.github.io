86b52b893efca715f2e87dddfce5379c843e5039
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-09-23 21:35:12 +0900

    fixed

--- scrollColors/scripts/scrollColors.js
+++ scrollColors/scripts/scrollColors.js
@@ -89,7 +89,7 @@
   window.addEventListener('scroll', function() {
     y = window.pageYOffset;
     dy = document.body.clientHeight;
-    if (y > dy - (dy / 2) && flg === false) {
+    if (y > dy - (dy / 3) && flg === false) {
       flg = true;
       makeDiv();
       removeDiv();
@@ -106,9 +106,11 @@
 
   window.addEventListener('resize', function() {
     let X = window.innerWidth;
+    /*
     if (X < 500) {
       return;
     }
+    */
     H = window.innerHeight; 
     for (let i = 0; i < divs.length; i++) {
       divs[i].style.height = H + 'px';
