75f3c3e19784c710a70c4d73c2a1cced2543c2b1
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2020-09-25 14:09:20 +0900

    fixed

--- scrollColors/scripts/scrollColors.js
+++ scrollColors/scripts/scrollColors.js
@@ -3,6 +3,7 @@
 * Created Date / Sep 23, 2020
 * Aurhor / Toshiya Marukubo
 * Twitter / https://twitter.com/toshiyamarukubo
+* Referenced / about change color https://yanohirota.com/color-converter/
 */
 
 (function() {
@@ -16,15 +17,17 @@
   let dy;
   
   // init 
-  makeDiv('rgb(255, 255, 255)', '#ffffff');
+  makeDiv('rgb(255, 255, 255)', 'hsl(0, 0%, 100%)', '#ffffff');
 
   // creat div
-  function makeDiv(rgb, hex) {
+  function makeDiv(rgb, hsl, hex) {
     const div = document.createElement('div');
     const ul = document.createElement('ul');
     const aR = document.createElement('a');
+    const aHS = document.createElement('a');
     const aH = document.createElement('a');
     const rgbL = document.createElement('li');
+    const hslList = document.createElement('li');
     const hexL = document.createElement('li');
     const r = Math.floor(Math.random() * 255 + 1);
     const g = Math.floor(Math.random() * 255 + 1);
@@ -32,17 +35,39 @@
     const hexR = Number(r).toString(16);
     const hexG = Number(g).toString(16);
     const hexB = Number(b).toString(16);
+    // hsl
+    const MAX = Math.max(r, g, b);
+    const MIN = Math.min(r, g, b);
+    let H;
+    if (r === MAX) H = 60 * ((g - b) / (MAX - MIN));
+    if (g === MAX) H = 60 * ((b - r) / (MAX - MIN)) + 120;
+    if (b === MAX) H = 60 * ((r - g) / (MAX - MIN)) + 240;
+    if (r === g === b) H = 0;
+    if (H < 0) H += 360;
+    let hslH = H.toFixed(0);
+    let S = (MAX + MIN) / 2;
+    if (S <= 127) S = (MAX - MIN) / (MAX + MIN);
+    if (S >= 128) S = (MAX - MIN) / (510 - MAX - MIN);
+    let hslS = (S * 100).toFixed(0);
+    let L = ((MAX + MIN) / 2) * (100 / 255);
+    let hslL = L.toFixed(0); 
     let rgbText = rgb || 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    let hslText = hsl || 'hsl(' + hslH + ', ' + hslS + '%, ' + hslL + '%)';
     let hexText = hex || '#' + hexR + hexG + hexB;
     aR.textContent = rgbText;
+    aHS.textContent = hslText;
     aH.textContent = hexText;
     aR.setAttribute('href', '#');
+    aHS.setAttribute('href', '#');
     aH.setAttribute('href', '#');
     rgbL.setAttribute('class', 'color');
+    hslList.setAttribute('class', 'color');
     hexL.setAttribute('class', 'color');
     rgbL.appendChild(aR);
+    hslList.appendChild(aHS);
     hexL.appendChild(aH);
     ul.appendChild(rgbL);
+    ul.appendChild(hslList);
     ul.appendChild(hexL);
     div.appendChild(ul);
     div.style.background = rgbText;
@@ -137,7 +162,8 @@
   
   button.addEventListener('click', function() {
     const rgb = document.getElementsByClassName('color')[0];
-    const code = document.getElementsByClassName('color')[1];
+    const hsl = document.getElementsByClassName('color')[1];
+    const hex = document.getElementsByClassName('color')[2];
     const li = document.createElement('li');
     const a = document.createElement('a');
     const ol = document.getElementsByTagName('ol')[0];
@@ -147,14 +173,18 @@
         return;
       }
     }
+    if (lists.length > 9) {
+      lists[0].parentNode.removeChild(lists[0]);
+    }
     a.setAttribute('href', '#');
+    a.textContent = ' ';
     li.appendChild(a);
     li.setAttribute('class', 'appear');
     li.style.background = rgb.textContent;
     fav.appendChild(li);
     li.addEventListener('click', function(e) {
       e.preventDefault();
-      makeDiv(rgb.textContent, code.textContent);
+      makeDiv(rgb.textContent, hsl.textContent, hex.textContent);
       removeDiv();
     }, false);
   }, false);
