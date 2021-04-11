eb2607b31d46278c8984510efae33ac7d44bd923
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2021-04-11 19:03:01 +0900

    fixed files

--- pattern/scripts/loaded.js
+++ pattern/scripts/loaded.js
@@ -1,7 +1,9 @@
 (function() {
   'use strict';
   var loading = document.getElementsByClassName('loading')[0];
+  var canvas = document.getElementById('canvas');
   window.addEventListener('load', function() {
+    canvas.setAttribute('class', 'show');
     setTimeout(function() {
       loading.parentNode.removeChild(loading);
     }, 1600);
