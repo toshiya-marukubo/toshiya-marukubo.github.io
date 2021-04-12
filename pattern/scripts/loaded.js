3a0a366be91cdffda9f815c65ca857b1ef503b92
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2021-04-12 13:37:21 +0900

    updated files

--- pattern/scripts/loaded.js
+++ pattern/scripts/loaded.js
@@ -2,8 +2,10 @@
   'use strict';
   var loading = document.getElementsByClassName('loading')[0];
   var canvas = document.getElementById('canvas');
+  var title = document.getElementById('title');
   window.addEventListener('load', function() {
     if (canvas) canvas.setAttribute('class', 'show');
+    if (title) title.setAttribute('class', 'show');
     setTimeout(function() {
       loading.parentNode.removeChild(loading);
     }, 1600);
