08a2a04500a4404341d09624f25812a23f0a1f5a
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2021-04-13 16:40:38 +0900

    fixed

--- pattern/scripts/displayImage.js
+++ pattern/scripts/displayImage.js
@@ -1,8 +1,8 @@
 (function() {
   'use strict';
   var H = window.innerHeight;
+  var imgs = document.getElementsByTagName('img');
   var displayImage = function () {
-    var imgs = document.getElementsByTagName('img');
     for (var i = 0; i < imgs.length; i++) {
       var s = imgs[i].getBoundingClientRect().top - 100;
       if (H > s) {
