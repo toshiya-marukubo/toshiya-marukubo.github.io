3019db699db7044d50f693be120b3bdd9941dbfa
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2021-11-21 07:28:01 +0900

    fixed

--- scripts/trashes.js
+++ scripts/trashes.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('load', function () {
   var getJSON = getItemsDataFromJSON;
   getJSON.initialize('codepen.json');
 
