de6405fa149fd5733a40b4c08aea05aa6cd8624e
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2021-11-21 20:29:52 +0900

    fixed

--- scripts/index.js
+++ scripts/index.js
@@ -109,7 +109,8 @@ Sketch.prototype.draw = function (frame) {
 
     var hamburger = document.getElementById('hamburger');
     var started = false;
-    hamburger.addEventListener('touchstart', function () {
+    hamburger.addEventListener('touchstart', function (e) {
+      e.preventDefault();
       if (started) return;
       started = true;
       this.classList.add('active');
