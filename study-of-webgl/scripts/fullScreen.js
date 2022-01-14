872647b341789d34db62caeed444d72e37bbcccc
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2022-01-14 12:55:49 +0900

    fixed

--- study-of-webgl/scripts/fullScreen.js
+++ study-of-webgl/scripts/fullScreen.js
@@ -1,8 +1,8 @@
 (() => {
   window.addEventListener('load', () => {
-    //console.log(visualViewport.height, window.innerHeight);
     let vh = visualViewport.height * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    
     window.addEventListener('resize', () => {
       let vh = visualViewport.height * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
