1e6f115265b011a0c850e3e7508ceec0012d03f3
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2022-08-13 21:58:34 +0900

    fixed

--- src/scripts/dailymotion-index.js
+++ src/scripts/dailymotion-index.js
@@ -2,6 +2,7 @@
 import '../styles/dailymotion.styl';
 
 // scripts
+import { FullScreen } from './pages/dailymotion/modules/full-screen';
 import { DisplayIframe } from './pages/dailymotion/modules/display-iframe';
 import { Sketch } from './pages/dailymotion/index/sketch';
 
@@ -10,6 +11,7 @@ import { Sketch } from './pages/dailymotion/index/sketch';
     const loading = document.getElementsByClassName('loading')[0];
     loading.classList.add('loaded');
     
+    const F = new FullScreen(false);
     const a = new DisplayIframe();
     const s = new Sketch();
   });
